04282405d3fa2b3c53fd810290aeac80439aa031
Chuck McAndrew <chuck.mcandrew@leblibrary.com>
2015-01-20 12:05:11 -0500

    added bootstrap

--- application/views/header.php
+++ application/views/header.php
@@ -1,16 +1,23 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');?>
 <!doctype html>
-<html>
+<html lang="en">
 	<head>
 		<title>Lebanon Libraries</title>
 		<script type="text/javascript" language="Javascript" 
 				src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js">
 		</script>
 		
-		<link rel="stylesheet" type="text/css" href='<?php echo base_url();?>assets/style.css'>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href='<?php echo site_url(array("assets","style.css"));?>'>
 		
 	</head>
 	<body>
-	<div class="nav">
-	<a href='<?php echo base_url(); ?>'> Home </a> | <a href='<?php echo base_url(); ?>hours/add_data/'> Add New Data</a> | <a href='<?php echo site_url(array('hours','older')) ?>'> View Older </a>
-	</div> 
+	<nav class='navbar'>
+		<div class='container'>
+			<ul class="nav nav-tabs">
+				<li><a href='<?php echo base_url(); ?>'> Home </a> </li>
+				<li><a href='<?php echo base_url(); ?>hours/add_data/'> Add New Data</a></li>
+				<li><a href='<?php echo site_url(array('hours','older')) ?>'> View Older </a></li>
+			</ul>
+		</div> 
+	</nav>
--- application/views/hours.php
+++ application/views/hours.php
@@ -1,5 +1,6 @@
 
-	<div id="search" class='left'>
+<div class='container'>
+	<div id="search" class='col-md-6'>
 	
 			<h1>Search Database</h1>
 			<h2>Search By Employee</h2>
@@ -7,7 +8,8 @@
 			<form method="post" action="<?php echo site_url($url)?>">
 					<select name="idEmployee">
 						<?php foreach ($name as $n):
-						echo "<option value=" . $n['idEmployee'] . ">" . $n['first_name'] . " " .$n['last_name'] . '</option>'; 
+						echo "<option value=" . $n['idEmployee'] . ">" . $n['first_name'] . " " .$n['last_name'] . '</option>
+						'; 
 						
 						endforeach;?>
 					</select> 
@@ -33,7 +35,8 @@
 					<select name="pay_period">
 					<?php 
 						foreach ($period as $p):
-							echo "<option>" . $p['pay_period'] . "</option>"; 
+							echo "<option>" . $p['pay_period'] . "</option>
+							"; 
 						endforeach;
 					?>
 					</select>
@@ -42,7 +45,7 @@
 				
 				</div>
 				
-				<div id="summary" class="center">
+				<div id="summary" class="col-md-6">
 				<h1>Summary</h1>
 				<h2>Totals for <?php echo $year ?></h2>
 					Total wages paid to date: <?php echo "$" . $totals['0']['total_wages'];?> <br/>
@@ -70,10 +73,13 @@
 				?>
 				
 				
-					
-		</body>
-	</div>
+				</div>	
+		</div>	
+	</body>
+	
 </html>
+
+<script src="<?php echo base_url();?>js/bootstrap.min.js"></script>
 
 
 
